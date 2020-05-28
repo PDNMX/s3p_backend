6fd33fead5f849878cbcd65c0047eb52a0994e08
IselaMorquecho <isela.escom@gmail.com>
2020-05-27 21:55:25 -0500

    origin

--- routes/API/v1.js
+++ routes/API/v1.js
@@ -3,15 +3,8 @@ var cors = require ('cors');
 
 var router = express.Router();
 
-var whitelist = ['https://plataformadigitalnacional.org/']
 var corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
+    origin: '*'
 }
 router.use(cors(corsOptions));
 
