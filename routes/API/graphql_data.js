075bfb4ea25b8e4007da4f0127b83402a304fb98
rosa isela morquecho <isela.escom@gmail.com>
2021-11-11 09:58:28 -0600

    fix graph

--- routes/API/graphql_data.js
+++ routes/API/graphql_data.js
@@ -1,63 +1,52 @@
-import ApolloClient, {InMemoryCache} from "apollo-boost";
-import {gql} from "apollo-boost";
-import 'cross-fetch/polyfill';
-
+const axios = require('axios');
 const GQL_REQUEST_TIMEOUT = parseInt(process.env.GQL_REQUEST_TIMEOUT || 30000, 10);
 
 const fetchEntities = endpoint => {
-    const client = new ApolloClient({
-        uri: endpoint.url,
-        timeout: GQL_REQUEST_TIMEOUT,
-        cache: new InMemoryCache({
-            addTypename: false
-        }),
-        headers: {
-            authorization: endpoint.token
-        }
-    });
-
-    return new Promise((resolve, reject) => {
-        client.query({
-            query: gql`
+    const query = `
                 query busca{
                     results_dependencias (ordenCampo:nombre, ordenSentido:desc){
                         nombre
                     }
                 }
-            `,
-            fetchPolicy: 'no-cache',
-        }).then(data => {
-            try {
+            `;
+    const opts = {
+        url: endpoint.url,
+        method: "POST",
+        timeout: GQL_REQUEST_TIMEOUT,
+        headers: {
+            authorization: endpoint.token
+        },
+        data: JSON.stringify({
+            query: query,
+            variables: {},
+        })
+    };
+
+    return new Promise((resolve, reject) => {
+        try {
+            return axios(opts).then(response => {
+                let {data} = response;
                 const entities = data.data.results_dependencias.map(e => {
                     e.supplier_id = endpoint.supplier_id;
                     return e;
                 });
                 resolve(entities);
-            } catch (e) {
-                console.log("Error:", e);
-                resolve([]);
-            }
-        }).catch(error =>{
-            console.log("Error",error)
-            resolve([])
-        })
+            }).catch(e => {
+                console.log(e);
+                resolve([])
+            })
+        } catch (e) {
+            console.log("Error:", e);
+            resolve([]);
+        }
+
     })
 };
 
 const fetchData = (endpoint, options) => {
     const {pageSize, page, query, sort} = options;
 
-    const client = new ApolloClient({
-        uri: endpoint.url,
-        cache: new InMemoryCache({
-            addTypename: false
-        }),
-        headers: {
-            authorization: endpoint.token
-        }
-    });
-
-    let gql_query = gql`
+    let gql_query = `
         query busca($filtros : FiltrosInput, $limit : Int, $offset : Int, $ordenCampo: ORDEN_CAMPO, $ordenSentido: ORDEN_SENTIDO){
             results(filtros: $filtros, limit: $limit, offset : $offset, ordenCampo: $ordenCampo, ordenSentido:$ordenSentido){
                 fecha_captura
@@ -104,8 +93,8 @@ const fetchData = (endpoint, options) => {
         query.nombre_razon_social = query.nombreRazonSocial;
         delete (query.nombreRazonSocial)
     }
-    if (query.hasOwnProperty('tipoPersona')){//2 moral 1-fisica
-        query.idtipo_persona = query.tipoPersona ==="F" ? "1" : "2";
+    if (query.hasOwnProperty('tipoPersona')) {//2 moral 1-fisica
+        query.idtipo_persona = query.tipoPersona === "F" ? "1" : "2";
         delete (query.tipoPersona)
     }
     if (query.hasOwnProperty('expediente')) {
@@ -122,7 +111,7 @@ const fetchData = (endpoint, options) => {
             query.cve_tipo_sancion.push("I");
         if (query.tipoSancion.includes("M"))
             query.cve_tipo_sancion.push("E");
-        switch (query.cve_tipo_sancion.length){
+        switch (query.cve_tipo_sancion.length) {
             case 0:
                 delete (query.cve_tipo_sancion)
                 break;
@@ -151,30 +140,44 @@ const fetchData = (endpoint, options) => {
         }
     }
 
-    return new Promise((resolve, reject) => {
-        client.query({
+    const opts = {
+        url: endpoint.url,
+        method: 'POST',
+        timeout: GQL_REQUEST_TIMEOUT,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            authorization: endpoint.token
+        },
+        data: JSON.stringify({
             query: gql_query,
             variables: variables,
-            fetchPolicy: 'no-cache'
-        }).then(result => {
-            let {data} = result;
-            data.supplier_name = endpoint.supplier_name;
-            data.supplier_id = endpoint.supplier_id;
-            data.levels = endpoint.levels;
-            data.endpoint_type = endpoint.type;
-            data.pagination = {
-                pageSize: pageSize,
-                page: page,
-                totalRows: parseInt(data.total)
-            };
-            resolve(data);
-        }).catch(error => {
-            console.log("Error: ", error);
-            reject(error)
         })
+    };
+
+    return new Promise((resolve, reject) => {
+        return axios(opts).then(response => {
+            const {data} = response;
+            try {
+                data.supplier_name = endpoint.supplier_name;
+                data.supplier_id = endpoint.supplier_id;
+                data.levels = endpoint.levels;
+                data.endpoint_type = endpoint.type;
+                data.pagination = {
+                    pageSize: pageSize,
+                    page: page,
+                    totalRows: parseInt(data.total)
+                };
+                resolve(data);
+            } catch (e) {
+                console.log("Error: ", e);
+                reject(e)
+            }
+        }).catch(e => {
+            console.log("Error: ", e);
+            reject(e)
+        });
     });
-
-
 };
 
 module.exports = {
